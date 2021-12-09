ad8e5defa3d8e6ce954c70886c6428356e27a6cd
Tosho Todorovski <tose_todorovski@yahoo.com>
2021-12-09 22:11:07 +0100

    Add day 1 to day 3

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdventOfCode2021;
+
+Console.WriteLine("Hello, Advent Of Code Fans!");
+
+
+Console.WriteLine("\nDay 1");
+
+Console.WriteLine("Part 1: " + Day1.Part1GetDepthMeasurmentIncreases(Day1.Input));
+Console.WriteLine("Part 2: " + Day1.Part2GetSumOfMeasurmentsIncreases(Day1.Input));
+
+Console.WriteLine("\nDay 2");
+
+Console.WriteLine("Part 1: " + Day2.Part1GetProductOfHorizonthalAndDepthPosition(Day2.Input));
+Console.WriteLine("Part 2: " + Day2.Part2GetProductOfFinalHorizonthalAndFinalDepthPosition(Day2.Input));
+
+Console.WriteLine("\nDay 3");
+
+Console.WriteLine("Part 1: " + Day3.Part1GetPowerConsumption(Day3.Input));
+Console.WriteLine("Part 2: " + Day3.Part2GetLifeSupportRating(Day3.Input));

f3624dbeee8fbce3b87fd03cfbf4135429e53b4f
Tosho Todorovski <tose_todorovski@yahoo.com>
2021-12-10 02:45:55 +0100

    Add day 4 to day 5 part 1

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -17,3 +17,12 @@ Console.WriteLine("\nDay 3");
 
 Console.WriteLine("Part 1: " + Day3.Part1GetPowerConsumption(Day3.Input));
 Console.WriteLine("Part 2: " + Day3.Part2GetLifeSupportRating(Day3.Input));
+
+Console.WriteLine("\nDay 4");
+
+Console.WriteLine("Part 1: " + Day4.Part1GetFinalScoreOfWinningBoard(Day4.Input, Day4.DrawnNumbers));
+Console.WriteLine("Part 2: " + Day4.Part2GetFinalScoreOfLastWinningBoard(Day4.Input, Day4.DrawnNumbers));
+
+Console.WriteLine("\nDay 5");
+
+Console.WriteLine("Part 1: " + Day5.Part1GetTwoLinesOverlapCount(Day5.Input));
